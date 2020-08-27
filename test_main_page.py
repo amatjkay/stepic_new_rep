aee127455ac8bbb87e042f74b5aeaf89b7846813
avkorchagin <amatjkay@gmail.com>
2020-08-27 16:42:01 +0300

    Добавлен метод is_element_present для обрботки ошибокпри поискеэлементов.

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+
 from .pages.main_page import MainPage
 
 
@@ -6,3 +8,14 @@ def test_guest_can_go_to_login_page(browser):
 	page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 	page.open()  # открываем страницу
 	page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+
+
+def test_guest_should_see_login_link(browser):
+	link = "http://selenium1py.pythonanywhere.com/"
+	page = MainPage(browser, link)
+	page.open()
+	page.should_be_login_link()
+
+
+def should_be_login_link(self):
+	assert self.is_element_present(By.CSS_SELECTOR, "#login_link_"), "Login link is not presented"
